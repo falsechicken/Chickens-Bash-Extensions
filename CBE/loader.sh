90e45d34d02b813d2c67a7164cf9d239c58be64e
False_Chicken <jmdevsupport@gmail.com>
2015-10-14 14:01:22 -0400

    - Initial Commit.

---
+++ CBE/loader.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+
+## USER SET
+CBE_CORE_INSTALL_PATH="/path/to/CBE"
+
+
+CBE_CORE_MODULES_PATH="$CBE_CORE_INSTALL_PATH"/modules
+
+CBE_CORE_BOOL_QUITEMODE=""
+CBE_CORE_BOOL_FLATFILE=""
+
+function CBE.Loader.ShowIntro()
+{
+	CBE.Loader.PrintMessageNewLine "##"
+	CBE.Loader.PrintMessageNewLine "# == Chicken's Bash Extensions =="
+	CBE.Loader.PrintMessageNewLine "#"
+	CBE.Loader.PrintMessageNewLine "# - All extension commands are prefixed with '@'. -"
+	CBE.Loader.PrintMessageNewLine "#"
+}
+
+## Initialize Modules
+function CBE.Loader.LoadModules()
+{
+	## Check if we are using flatfile mode
+	if [ "$CBE_CORE_BOOL_FLATFILE" == "true" ]; then
+		CBE.Loader.LoadFlatFile
+	fi
+	
+	CBE.Loader.PrintMessageNewLine "# - Loading Modules - "
+	
+	CBE_CORE_TMP_LAST_DIR="$(pwd)"
+		
+	cd "$CBE_CORE_MODULES_PATH"
+	
+	for f in *.cbe; do 
+		if [ "$f" == "*.cbe" ]; then
+			CBE.Loader.PrintMessageNewLine "# ERROR: No modules found!"
+		else
+			CBE.Loader.PrintMessageNewLine "# -- Processing module: ${f%.*}"
+			. "$f"
+		fi
+	done
+	
+	cd "$CBE_CORE_TMP_LAST_DIR"
+	
+	CBE.Loader.PrintMessageNewLine "# - Module Load Complete -"
+}
+
+function CBE.Loader.LoadFlatFile()
+{
+	CBE.Loader.PrintMessage "# Loading flatfile..." 
+	
+	if [ -e ~/.cbe_dir ]; then
+		CBE_CORE_INSTALL_PATH=$(head -n 1 ~/.cbe_dir)
+		CBE_CORE_MODULES_PATH="$CBE_CORE_INSTALL_PATH"/modules ## Reset the modules dir with new install path from flatfile.
+		CBE.Loader.PrintMessageNewLine "OK!"
+	else
+		CBE.Loader.PrintMessageNewLine "Fail!"
+		CBE.Loader.PrintMessageNewLine "# (WARN) Flat file mode enabled but no file exists! (~/.cbe_dir)"
+	fi
+}
+
+function CBE.Loader.ShowLoadingComplete()
+{
+	CBE.Loader.PrintMessageNewLine "##"
+}
+
+function CBE.Loader.PrintMessageNewLine()
+{
+	if [ "$CBE_CORE_BOOL_QUITEMODE" != "true" ]; then
+		echo "$1"
+	fi
+}
+
+function CBE.Loader.PrintMessage()
+{
+	if [ "$CBE_CORE_BOOL_QUITEMODE" != "true" ]; then
+		echo -n "$1"
+	fi
+}
+
+function CBE.Loader.SetOptions()
+{
+	CBE_CORE_BOOL_QUITEMODE="$1"
+	CBE_CORE_BOOL_FLATFILE="$2"
+}
+
+#
+# Clean up references to functions and vars the user should not have access to.
+##
+function CBE.Loader.CleanUp()
+{
+	unset -f CBE.Loader.ShowIntro
+	unset -f CBE.Loader.LoadModules
+	unset -f CBE.Loader.ShowLoadingComplete
+	unset -f CBE.Loader.PrintMessageNewLine
+	unset -f CBE.Loader.PrintMessage
+	unset -f CBE.Loader.LoadFlatFile
+	unset -f CBE.Loader.SetOptions
+
+	unset CBE_CORE_TMP_LAST_DIR	
+	unset CBE_CORE_BOOL_QUITEMODE
+	unset CBE_CORE_BOOL_FLATFILE
+}
+
+## FUNCTION CALLS
+
+CBE.Loader.SetOptions "$1" "$2"
+CBE.Loader.ShowIntro
+CBE.Loader.LoadModules
+CBE.Loader.ShowLoadingComplete
+CBE.Loader.CleanUp
+
+## END FUNCTION CALLS
+
+unset -f CBE.Loader.CleanUp
---
+++ README.txt
@@ -0,0 +1,18 @@
+= Chicken's Bash Extensions =
+
+"Plug-ins" for Bash.
+
+== TO USE ==
+
+Insert ". /path/to/cbe/loader.sh false false" into the bottom of your .bashrc file and edit the CBE_CORE_INSTALL_PATH in the loader.sh script to your CBE folder 
+
+OR
+
+Insert ". /path/to/cbe/loader.sh false true" and create the file ~/.cbe_dir and add the CBE install path to the first line.
+
+
+Loader usage: loader.sh <quite mode> <flatfile mode>
+
+Quite Mode: Disable startup loading messages.
+
+FlatFile Mode: Pull the CBE_CORE_INSTALL_PATH from ~/.cbe_dir instead of the hard coded script variable.
